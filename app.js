ce4eb386a12d52dd9c8f6e29de15d7af466ceb5b
Christian Dorado <christiandaviddorado@MacBook-Pro-de-Christian-Dorado.local>
2018-10-29 00:30:21 +1100

    Add cors
    Add cors

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const api = require('./routes')
+const cors = require('cors')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use('/api', api)
+app.use(cors())
 
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@
 const express = require('express')
 const api = express.Router()
 const feedCtrl = require('../controllers/feed')
+const cors = require('cors')
 
+api.use(cors())
 api.get('/feed', feedCtrl.feed)
-
 api.get('/ping', function(req, res) {
     res.status(200).json('pong');
 });
